fa9bbe30b4f9e5077d35b51d93e9f694a4001682
marcocannici <marco.cannici@mail.polimi.it>
2020-10-08 14:24:49 +0000

    Update requirements and setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as fh:
+    requirements = fh.read().splitlines()
+
 setuptools.setup(
     name="yapt",
     version="0.0.1",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://gitlab.com/mciccone/yapt",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
